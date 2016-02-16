4aa31875bd8725bd47f46effee332b828029ef58
Alexander Sopov <sopovalexander@gmail.com>
2016-02-16 19:14:02 +0100

    created repo

---
+++ core/src/se/swedishcoffee/game/Game.java
@@ -0,0 +1,35 @@
+package se.swedishcoffee.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Game extends ApplicationAdapter {
+	SpriteBatch batch;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.end();
+	}
+
+	@Override
+	public void pause () {
+	}
+
+	@Override
+	public void resume () {
+	}
+
+	@Override
+	public void dispose () {
+	}
+}
